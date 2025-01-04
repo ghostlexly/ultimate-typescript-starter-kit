ed056788ffdd42c7b7f58e1d392645a1f0e8df63
ghostlexly <ghostlexly@gmail.com>
2025-01-04 22:10:32 +0100

    upd eslint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+import jsPlugin from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest },
+      parser: tsParser,
+    },
+    plugins: {
+      js: jsPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...jsPlugin.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
