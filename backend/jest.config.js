b3dae38d56fa2546ed7160648602109244158d41
ghostlexly <ghostlexly@gmail.com>
2025-01-12 18:48:12 +0100

    update the tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,3 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   moduleFileExtensions: ["js", "json"],
   rootDir: "dist/src",
--- backend/src/modules/test/tests/test.controller.test.ts
+++ backend/src/modules/test/tests/test.controller.test.ts
@@ -10,13 +10,28 @@ beforeAll(async () => {
 
 describe("AdminCartController", () => {
   describe("GET /api/tests/bad-request", () => {
-    it("should return empty cart list", async () => {
+    it("returns a status code 400 if the bad request test is successful", async () => {
       const response = await request(app).get("/api/tests/bad-request");
 
       expect(response.status).toEqual(400);
       expect(response.body).toEqual({
         message: "An error occurred.",
         code: "TEST_BAD_REQUEST",
+      });
+    });
+  });
+
+  describe("GET /api/tests/dependency-injection", () => {
+    it("returns a status code 200 if the dependency injection is successful", async () => {
+      const response = await request(app).get(
+        "/api/tests/dependency-injection"
+      );
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({
+        commissionRate: 25,
+        message:
+          "message received from another module's injected service: Hello World",
       });
     });
   });
