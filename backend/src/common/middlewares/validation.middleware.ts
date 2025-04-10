5bbbc33ae0fffd4a296dc6b0b14fdbcb28de47ee
ghostlexly <ghostlexly@gmail.com>
2025-04-11 01:01:11 +0200

    upd validation middleware types

--- backend/src/common/middlewares/validation.middleware.ts
+++ backend/src/common/middlewares/validation.middleware.ts
@@ -1,6 +1,7 @@
 import { AnyZodObject, ZodEffects, ZodError } from "zod";
 import { Request, Response, NextFunction } from "express";
 import { ValidationException } from "@/common/exceptions/validation-exception";
+import { z } from "zod";
 
 /**
  * Validates request query, params and body parameters
@@ -47,10 +48,12 @@ export const validateRequest =
  * Validates data without middleware functionality
  * @param zodSchema Zod validator that will be used to validate the data
  * @param data Object containing body, query, params, file, files to validate
- * @returns The validated data
+ * @returns The validated data with proper typing from the schema
  */
-export const validateData = async (
-  zodSchema: AnyZodObject | ZodEffects<AnyZodObject>,
+export const validateData = async <
+  T extends AnyZodObject | ZodEffects<AnyZodObject>
+>(
+  zodSchema: T,
   data: {
     body?: any;
     query?: any;
@@ -58,11 +61,10 @@ export const validateData = async (
     file?: any;
     files?: any;
   }
-) => {
+): Promise<z.infer<T>> => {
   try {
     // Validate the provided data
-    const validatedData = await zodSchema.parseAsync(data);
-
+    const validatedData = (await zodSchema.parseAsync(data)) as z.infer<T>;
     return validatedData;
   } catch (error) {
     if (error instanceof ZodError) {
