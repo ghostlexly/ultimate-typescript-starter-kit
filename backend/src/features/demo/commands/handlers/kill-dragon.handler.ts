2e19ddeb2c910e3082299aac33e0b466d6676884
ghostlexly <ghostlexly@gmail.com>
2025-10-15 22:37:34 +0200

    upd handler args

--- backend/src/features/demo/commands/handlers/kill-dragon.handler.ts
+++ backend/src/features/demo/commands/handlers/kill-dragon.handler.ts
@@ -3,9 +3,9 @@ import { KillDragonCommand } from '../impl/kill-dragon.command';
 
 @CommandHandler(KillDragonCommand)
 export class KillDragonHandler implements ICommandHandler<KillDragonCommand> {
-  async execute(command: KillDragonCommand) {
+  async execute({ dragonId, heroId }: KillDragonCommand) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    return { dragonId: command.dragonId, heroId: command.heroId, killed: true };
+    return { dragonId, heroId, killed: true };
   }
 }
