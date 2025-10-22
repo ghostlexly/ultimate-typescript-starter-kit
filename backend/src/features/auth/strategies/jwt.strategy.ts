af58687f2b8a58ba1af8d318feff0b4f641b8c0b
ghostlexly <ghostlexly@gmail.com>
2025-10-22 15:54:51 +0200

    add jwt account session to the cache

--- backend/src/features/auth/strategies/jwt.strategy.ts
+++ backend/src/features/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,17 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
-import { jwtConstants } from '../auth.constants';
+import { Inject, Injectable } from '@nestjs/common';
+import { authConstants, jwtConstants } from '../auth.constants';
 import { DatabaseService } from 'src/features/application/services/database.service';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+import { Account } from 'src/generated/prisma/client';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private db: DatabaseService) {
+  constructor(
+    private db: DatabaseService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         // Extract the token from the URL query parameter
@@ -30,7 +35,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     try {
       const sessionId = payload.sub;
 
-      // Get account by id
+      // Get account by session id from cache
+      const cachedAccount = await this.cacheManager.get<Account>(
+        `session:${sessionId}`,
+      );
+
+      // If the account is found in the cache, return it
+      if (cachedAccount) {
+        return cachedAccount;
+      }
+
+      // If the account is not found in the cache, get it from the database
       const account = await this.db.prisma.account.findFirst({
         include: {
           admin: true,
@@ -39,9 +54,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         where: { sessions: { some: { id: sessionId } } },
       });
 
+      // If the account is not found in the database, return false (authentication failed)
       if (!account) {
         return false;
       }
+
+      // If the account is found in the database, cache it
+      await this.cacheManager.set(
+        `session:${sessionId}`,
+        account,
+        authConstants.accessTokenExpirationMinutes * 60 * 1000, // Convert minutes to milliseconds
+      );
 
       return account;
     } catch {
