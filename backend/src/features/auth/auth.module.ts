06e94f15683e4bcfd5504f78247e7ae9946fdf8b
ghostlexly <ghostlexly@gmail.com>
2025-10-22 16:20:15 +0200

    add clear expired sessions cron

--- backend/src/features/auth/auth.module.ts
+++ backend/src/features/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { jwtConstants } from './auth.constants';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ClearExpiredSessionsCron } from './crons/clear-expired-sessions.cron';
 
 @Module({
   imports: [
@@ -18,7 +19,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, ClearExpiredSessionsCron],
   exports: [AuthService],
 })
 export class AuthModule {}
