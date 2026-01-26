9fd5cee7158a0bf2f8f69f50a127845fe96e165c
Tolga MALKOC <t.malkoc@la-boite-immo.com>
2026-01-26 13:54:10 +0100

    feat: add `useFirstMount` hook for detecting initial component mount with optional callback functionality

---
+++ frontend/src/hooks/use-first-mount.ts
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+
+export function useFirstMount(callback?: () => void): boolean {
+  const isFirstMountRef = useRef(true);
+
+  if (isFirstMountRef.current) {
+    isFirstMountRef.current = false;
+    callback?.();
+
+    return true;
+  }
+
+  return false;
+}
