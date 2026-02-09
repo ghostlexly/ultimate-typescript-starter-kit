f1a314092f13c71cc346b690a8cb4e8cfe9ea3a1
Tolga MALKOC <t.malkoc@la-boite-immo.com>
2026-02-09 16:53:14 +0100

    refactor: set default `staleTime` for queries in `makeQueryClient` to improve SSR behavior

--- frontend/src/lib/query-client.ts
+++ frontend/src/lib/query-client.ts
@@ -7,10 +7,9 @@ import {
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
-      // queries: {
-      //   staleTime: 60 * 1000,
-      // },
       queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
         staleTime: 5 * 1000,
       },
       dehydrate: {
